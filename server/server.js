b8c2391b22e12586dae52463ce49b1736ead338a
Mark Washkewicz <mwashkewicz@gmail.com>
2020-11-13 16:58:36 -0800

    added server routes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const Redis = require('ioredis');
-
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
+
+app.use(bodyParser({ urlencoded: true }));
+app.use(express.json())
+
 const channel = "hi"
 //use to add subscriber to certain channel
 //for now always being added to channel "hi"
-app.use('/sub',(req, res)=>{
+app.use('/sub', (req, res) => {
     const redis = new Redis();
     redis.subscribe(channel, (error, count) => {
         if (error) {
@@ -23,20 +28,20 @@ app.use('/sub',(req, res)=>{
 
 //use to publish to channel
 //rn always publishing to "hi"
-app.use('/pub',(req, res)=>{
+app.use('/pub', (req, res) => {
     let redis = new Redis();
     redis.publish(channel, "hi")
     res.send('pub')
 })
 
 //get all channels
-app.use('/getchannels',(req, res)=>{
+app.use('/getchannels', (req, res) => {
     let redis = new Redis();
     redis.pubsub('channels', (err, channels) => {
         if (err) {
-          
+
         } else {
-          console.log('Channels:', channels); // array
+            console.log('Channels:', channels); // array
         }
     });
 
@@ -45,18 +50,34 @@ app.use('/getchannels',(req, res)=>{
 
 //get subscribers to certain channel
 //rn always hi
-app.use('/numsub',(req, res)=>{
+app.use('/numsub', (req, res) => {
     let redis = new Redis();
-    redis.pubsub('numsub', channel,(err, channels) => {
+    redis.pubsub('numsub', channel, (err, channels) => {
         if (err) {
-          
+
         } else {
-          console.log('Channels:', channels); // array
+            console.log('Channels:', channels); // array
         }
     });
 
     res.send('numsub')
 })
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+
+app.post('/login', (req, res) => {
+    //check the credenitals user and pw from req.body
+    //if the user is an admin --> redirect to adminview?
+    //if the user is a user --> redirect to userview?
+});
+
+app.get('/userview', /**send userid? and sendFile userview? */)
+
+app.get('/adminview', /**send userid? and sendFile adminview? */)
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
