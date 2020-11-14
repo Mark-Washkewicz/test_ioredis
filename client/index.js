11f6d55a1d6da3987ced7044b31362e0d0da6a60
Mark Washkewicz <mwashkewicz@gmail.com>
2020-11-13 16:39:45 -0800

    added webpack

---
+++ client/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+
+import styles from './styles.css'
+// uncomment so that webpack can bundle styles
+// import styles from './scss/application.scss';
+
+render(
+  <h1>asdasd</h1>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const { runInContext } = require('vm')
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: './client/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build/')
+    },
+    
+    devServer: {
+        publicPath: '/build/',
+        proxy: [{
+           
+                context: '/api/leaders',
+                target:'http://localhost:3000/'
+            
+        }]
+      },
+    
+      
+    
+    module:{
+
+        rules:[
+            {
+                test:/\.jsx?/,
+                exclude: /(node_modules)/,
+                
+                // query: {
+                //     presets: ['es2015']
+                // },
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  'style-loader',
+                  // Translates CSS into CommonJS
+                  'css-loader',
+                  // Compiles Sass to CSS
+                //   'sass-loader',
+                ],
+            },
+
+            {
+                test: /\.(jpg|jpeg|png)$/,
+                use: {
+                 loader: 'url-loader'
+                }
+            },
+
+        ]
+
+    }
+}
